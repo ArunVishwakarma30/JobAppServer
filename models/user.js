060d2abde56ad4fac1ccfefe95944ea73f70f125
Arun Vishwakarma <avishwakarma3009@gmail.com>
2023-12-31 13:17:25 +0530

    API BUG FIX

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const UserSchema = mongoose.Schema(
         loaction: { type: String, required: false },
         isAdmin: { type: Boolean, default: false },
         isAgent: { type: Boolean, default: false },
-        skills: { type: Array, default: false },
+        skills: { type: Array, required: false },
         profile: { type: String, requird: true, default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }
         
     },
